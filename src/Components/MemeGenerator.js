80cb63878f69d4639b00f61d21a189d88a693f26
abdullah-ch <abdullahchaudharry@gmail.com>
2020-04-17 20:25:26 +0500

    added Download Button

--- src/Components/MemeGenerator.js
+++ src/Components/MemeGenerator.js
@@ -7,16 +7,18 @@ export class MemeGenerator extends React.Component {
       topText: "",
       bottomText: "",
       randomImage: "http://i.imgflip.com/1bij.jpg",
-      allMemeImages: []
+      allMemeImages: [],
+      image: "http://i.imgflip.com/1bij.jpg",
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleDownload = this.handleDownload.bind(this);
   }
 
   componentDidMount() {
     fetch("https://api.imgflip.com/get_memes")
-      .then(response => response.json())
-      .then(response => {
+      .then((response) => response.json())
+      .then((response) => {
         const { memes } = response.data;
         this.setState({ allMemeImages: memes });
       });
@@ -25,21 +27,50 @@ export class MemeGenerator extends React.Component {
   handleChange(event) {
     const { name, value } = event.target;
     this.setState({
-      [name]: value
+      [name]: value,
     });
   }
 
   handleClick(event) {
     event.preventDefault();
-    const randindex = Math.floor(
-      Math.random() * 100
-    );
+    const randindex = Math.floor(Math.random() * 100);
     const RandomImageUrl = this.state.allMemeImages[randindex].url;
     this.setState({
-      randomImage: RandomImageUrl
+      randomImage: RandomImageUrl,
+      image: this.state.randomImage,
     });
+    console.log("Random immage", this.state.randomImage);
+    console.log("My download image url", this.state.image);
   }
 
+  handleDownload(event) {
+    event.preventDefault();
+    console.log("HREF", event.target.href);
+    console.log("My random image url", this.state.randomImage);
+    this.setState({
+      image: this.state.randomImage,
+    });
+    console.log("My download image url", this.state.image);
+    ///////////////////////////////////////////////////////
+    //
+    fetch(event.target.href, {
+      method: "GET",
+      headers: {},
+    })
+      .then((response) => {
+        response.arrayBuffer().then(function (buffer) {
+          const url = window.URL.createObjectURL(new Blob([buffer]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", "image.png"); //or any other extension
+          document.body.appendChild(link);
+          link.click();
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   render() {
     return (
       <div>
@@ -68,6 +99,10 @@ export class MemeGenerator extends React.Component {
             <h2 className="bottom">{this.state.bottomText}</h2>
           </div>
         </form>
+        <a href={this.state.randomImage} download onClick={this.handleDownload}>
+          <i className="fa fa-download" />
+          Download the MEME
+        </a>
       </div>
     );
   }
